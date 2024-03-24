7cff54909891da4b8e0a3114f72330444c5a6897
Kostya Cholak <kostya.cholak@gmail.com>
2024-03-24 19:46:22 +0100

    Add dict hierarchy

--- src/knowledge_base/hierarchy.py
+++ src/knowledge_base/hierarchy.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from src.knowledge_base.concept import Concept
 
 
@@ -9,7 +11,7 @@ class BaseHierarchy:
         pass
 
 
-class PlainHierarchy:
+class PlainHierarchy(BaseHierarchy):
     def get_parents(self, cid: str, include_self=True) -> list[str]:
         if not include_self:
             return []
@@ -19,6 +21,53 @@ class PlainHierarchy:
         if not include_self:
             return []
         return [Concept.get_name(cid), ]
+    
+    
+class DictHierarchy(BaseHierarchy):
+    def __init__(self, *, 
+                 children: dict[str, list[str]] = None,
+                 parents: dict[str, list[str]] = None):
+        if children is not None:
+            self.children = children
+            self.parents = self._invert_dict(children)
+        elif parents is not None:
+            self.parents = parents
+            self.children = self._invert_dict(parents)
+        elif children is not None and parents is not None:
+            raise ValueError("Provide only children or parents")
+        else:
+            raise ValueError("Provide children or parents")
+
+    def get_parents(self, cid: str, include_self=True) -> list[str]:
+        self_as_parent = [cid, ] if include_self else []
+        
+        try:
+            result = set(self_as_parent)
+
+            for parent in self.parents[cid]:
+                result.add(parent)
+                result.update(self.get_parents(parent))
+
+            return sorted(list(result))
+        except KeyError:
+            return self_as_parent
+
+    def get_children(self, cid: str, include_self=False) -> list[str]:
+        self_as_parent = [cid, ] if include_self else []
+        
+        try:
+            return self.children[cid] + self_as_parent
+        except KeyError:
+            return self_as_parent
+
+    @staticmethod
+    def _invert_dict(parents):
+        children = defaultdict(list)
+        for child, pars in parents.items():
+            for parent in pars:
+                children[parent].append(child)
+
+        return children
     
 
 def is_child(hierarchy: BaseHierarchy, concept: Concept, parent: Concept) -> bool:
