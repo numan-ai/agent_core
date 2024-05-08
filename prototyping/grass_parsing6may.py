d40dbc8c8537f9e2a67c90e91a933c83ebd218f9
Kostya Cholak <konstantin@mirato.com>
2024-05-08 23:06:33 +0200

    Add two energy layers

--- prototyping/grass_parsing6may.py
+++ prototyping/grass_parsing6may.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
+import math
+import random
+import time
 from typing import Hashable
 from grass import AssociativeEnergyGraph
 
 from src.knowledge_base.hierarchy import DictHierarchy
 
-# we can't take all concepts of any word.
-# because any word has a concept of "LiteralWord", which will make the parsing so mych slower.
+# we can't find all concepts of any word.
+# because any word has a concept of "LiteralWord", which will make the parsing so much slower.
 # or we can..
 
 sentence = ["Word_my", "Word_brother", "Word_is", "Word_running", ]
@@ -30,6 +33,9 @@ aeg = AssociativeEnergyGraph([
     ('Activity', 'LivingEntityIsDoingActivity', 1.0),
 ], bidirectional=False)
 
+global_level = aeg.add_energy_layer("global")
+local_level = aeg.add_energy_layer("local")
+
 patterns = {
     "LivingEntityIsDoingActivity": ["LivingEntity", "IsDoing", "Activity"],
     "NonLivingEntityIsDoingActivity": ["NonLivingEntity", "IsDoing", "Activity"],
@@ -49,10 +55,10 @@ pattern_nodes = None
 pattern_idx = 0
 
 for word_idx in range(len(sentence)):
-    aeg.add_energy(sentence[word_idx], energy=1.0, propagation=1.0)
+    local_level.add_energy(sentence[word_idx], energy=1.0, propagation=1.0)
     if word_idx < len(sentence) - 1:
         # look ahead
-        aeg.add_energy(sentence[word_idx + 1], energy=1.0, propagation=1.0)
+        local_level.add_energy(sentence[word_idx + 1], energy=1.0, propagation=1.0)
 
     concept = aeg.lookup(sentence[word_idx])[0][0]
     if pattern_nodes is None:
@@ -61,7 +67,7 @@ for word_idx in range(len(sentence)):
         pattern_nodes = patterns[pattern_concept]
 
         for pattern_node in pattern_nodes[1:]:
-            aeg.add_energy(pattern_node, energy=1.0, propagation=1.0)
+            local_level.add_energy(pattern_node, energy=1.0, propagation=1.0)
     else:
         node_concept = pattern_nodes[pattern_idx]
         if node_concept != concept:
@@ -82,7 +88,7 @@ for word_idx in range(len(sentence)):
         pattern_idx = 1
 
         for pattern_node in pattern_nodes[1:]:
-            aeg.add_energy(pattern_node, energy=1.0, propagation=1.0)
+            local_level.add_energy(pattern_node, energy=1.0, propagation=1.0)
 
 if len(pattern_nodes) != pattern_idx:
     print("Pattern NOT matched", pattern_name)
